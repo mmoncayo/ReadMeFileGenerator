037ac341433c1db92a233a2c14525dcd9bcbefbf
Matt Moncayo <matthew.moncayo@gmail.com>
2020-06-04 19:37:21 -0700

    updated last questions to be formatted properly

--- index.js
+++ index.js
@@ -14,23 +14,57 @@ function promptUser() {
             },
             {
                 type: "input",
-                message: "Where are you from?",
-                name: "location"
+                message: "What is your email?",
+                name: "email"
             },
             {
                 type: "input",
-                message: "What do you do for a living?",
-                name: "bio"
+                message: "What's the URL to your project?",
+                name: "url"
             },
             {
                 type: "input",
-                message: "What is your LinkedIn URL?",
-                name: "linkedin"
+                message: "What is your project's name?",
+                name: "project"
             },
             {
                 type: "input",
-                message: "What is your GitHub URL?",
-                name: "github"
+                message: "Please write a short description of your project",
+                name: "description"
+            },            
+            {
+                type: "list",
+                message: "What kind of license should your project have?",
+                name: "license",
+                choices: [
+                    "MIT",
+                    "APACHE 2.0",
+                    "GPL 3.0",
+                    "BSD 3",
+                    "None"
+                  ]
+            },
+            {
+                type: "input",
+                message: "What command should be ran to install dependencies?",
+                name: "installation",
+                default: "npm install"
+            },
+            {
+                type: "input",
+                message: "What command should be ran to run tests?",
+                name: "test",
+                default: "npm test"
+            },
+            {
+                type: "input",
+                message: "What does the user need to know about using the repo?",
+                name: "usage"
+            },
+            {
+                type: "input",
+                message: "What does the user need to know about contributing to the repo?",
+                name: "contribution"
             }
         ]);
 }
@@ -38,12 +72,12 @@ function promptUser() {
 function generateReadMe(answers) {
 
     return `
-    # test
-    [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/blah/test)
+    # ${answers.project}
+    [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://${answers.url})
     ​
     ## Description
     ​
-    project about blah
+    ${answers.description}
     ​
     ## Table of Contents 
     ​
@@ -64,32 +98,32 @@ function generateReadMe(answers) {
     To install necessary dependencies, run the following command:
     ​
     \`\`\`
-    npm i
+    ${answers.installation}
     \`\`\`
     ​
     ## Usage
     ​
-    user needs to know blah
+    ${answers.usage}
     ​
     ## License
     ​
-    This project is licensed under the MIT license.
+    This project is licensed under the ${answers.license} license.
       
     ## Contributing
     ​
-    user needs to contribute to blah
+    ${answers.contribution}
     ​
     ## Tests
     ​
     To run tests, run the following command:
     ​
     \`\`\`
-    npm test
+    ${answers.test}
     \`\`\`
     ​
     ## Questions
     ​
-    If you have any questions about the repo, open an issue or contact [blah](undefined) directly at blah@blah.com.`;
+    If you have any questions about the repo, open an issue or contact [${answers.github}](undefined) directly at ${answers.email}.`;
 }
 
 async function init() {
